d2a04f84d533c3c57d2f0071e6bfa9714bd317f4
reshalwarna <reshalfernando19@gmail.com>
2023-03-22 11:49:34 +0100

    Create ideal_data.m

---
+++ MATLAB/Funzioni/ideal_data.m
@@ -0,0 +1,24 @@
+function ideal = ideal_data(gamma, MM, T_amb, p_amb, p_e, A_e, p_c, A_t, u_e)
+
+R = 8314;  % [kJ / kg * K]
+g0 = 9.81  % [m/s^2]
+
+% Vandencherkove function
+GAMMA = sqrt(gamma.*(2/(gamma+1)).^((gamma+1)/(gamma-1)));
+
+% C_star
+c_star = (p_c .* A_t) ./ (GAMMA .* (p_c ./ (sqrt((R / MM).*T_c))) .* A_t);
+
+% CT_ideal
+ct_ideal = sqrt((2.*gamma.^2)/(gamma-1).*(2/(gamma+1)).^((gamma+1)/(gamma-1) .* ...
+            (1-(p_e/p_c).^((gamma-1)/gamma)))) + ((p_e - p_amb)/p_c).*(A_e/A_t);
+
+% IS_ideal
+IS_ideal = (ct_ideal .* c_star) ./ (g0);
+
+% Thrust
+T = mp * u_e + (p_e - p_amb) * A_e;
+
+% mass flowrate ideal
+m_p_dot = T ./ (IS_ideal .* g0);
+
