9531e4a202628670b04eafe56ef7e9284e2d8dd9
danieleP14 <115355495+danieleP14@users.noreply.github.com>
2023-03-22 16:47:10 +0100

    dati_fitti

--- MATLAB/Funzioni/AeAt_p.m
+++ MATLAB/Funzioni/AeAt_p.m
@@ -1,6 +1,6 @@
 clc; close all; clear;
 
-FILE = fopen("SI_C.txt", "r");
+FILE = fopen("data_AeAt.txt", "r");
 formatSpec = '%f';
 
 A = fscanf(FILE, formatSpec,[3,inf])';
@@ -12,7 +12,7 @@ Pe   = A(:, 2);
 Te   = A(:, 3);
 
 figure;
-plot(AeAt, Pe, 'o');
+plot(AeAt, Pe, '-o', "MarkerSize",3);
 grid on;
 grid minor;
 xlabel("Rapporto di Ae/At");
