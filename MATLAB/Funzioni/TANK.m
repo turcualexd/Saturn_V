6b4ad26bbeb7234d004748328896839091d00691
Silvietta25 <115651216+Silvietta25@users.noreply.github.com>
2023-04-06 10:10:22 +0200

    dimensionamento tank

---
+++ MATLAB/Funzioni/TANK.m
@@ -0,0 +1,31 @@
+clear; clc;
+
+m_RP1 = 635029.318; %kg 
+m_LOX = 1441516.552; %kg
+
+rho_LOX = 1141; % kg/m^3
+rho_RP1 = 810; % kg/m^3
+
+Vol_LOX = m_LOX/rho_LOX 
+Vol_RP1 = m_RP1/rho_RP1 
+
+
+
+r = 10.058/2;
+%h = 19.532;
+% Vol_r_LOX = pi*r^2*h 
+
+
+h_LOX = Vol_LOX/(pi*r^2)
+
+h_RP1 = Vol_RP1/(pi*r^2)
+
+
+
+
+
+
+
+
+
+
---
+++ MATLAB/Funzioni/untitled.m
@@ -0,0 +1,15 @@
+
+gamma = 
+MM = 
+T_amb = 
+p_amb = 
+T_c = 
+p_e = 
+A_e = 
+p_c = 
+A_t = 
+u_e = 
+
+
+
+[ct_ideal IS_ideal m_p_dot T M_e] = ideal_data(gamma, MM, T_amb, p_amb,T_c, p_e, A_e, p_c, A_t, u_e)
