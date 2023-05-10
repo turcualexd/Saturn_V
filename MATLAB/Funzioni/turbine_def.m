645ab5cb4addc8612e95efafb37d6f1cc637a905
danieleP14 <115355495+danieleP14@users.noreply.github.com>
2023-05-10 16:48:32 +0200

    funzione turbina + file latex: appendice triangoli velocità + immagini nuove qualita maggiore

--- MATLAB/Funzioni/turbine_def.m
+++ MATLAB/Funzioni/turbine_def.m
@@ -33,11 +33,14 @@ eps_blade = 0.95;   % stator and rotor blade exit area coefficient
 
 dh_tot     = c_p*T_gg * (1 - (1/eps)^((g-1)/g)); % salto totale turbina.
 dh_nz      = (1 - 0.06)*dh_tot;
+dh_rot1    = dh_tot*0.02;
+dh_stat    = dh_rot1;
 p_e_nzz    = p_in*(1 - dh_nz/(c_p*T_gg))^(g/(g-1));
 C_zero     = sqrt(2*dh_nz);
 C_1        = k_n * C_zero; 
 U          = C_zero*U_over_c0;
 
+U_vec       = U*[1, 0];
 q_n_re      = ((1 - k_n^2)*C_1^2)/(k_n^2);
 T_e_nzz_id  = T_gg - dh_nz/c_p;
 T_e_nzz_re  = T_e_nzz_id + q_n_re/c_p;
@@ -49,24 +52,26 @@ alfa_1_deg = alfa_1*(180/pi);
 beta_1     = atan(C_1*sin(alfa_1)/(C_1*cos(alfa_1) - U));
 beta_1_deg = rad2deg(beta_1);
 
-V_1        = C_1*sin(alfa_1)/sin(beta_1);
 
+C_1_vec    = C_1 * [cos(-alfa_1), sin(-alfa_1)];
+V_1_vec    = C_1_vec - U_vec;
+V_1        = norm(V_1_vec);
 
 %% solving system 
 % ipotesi --> mach uscita 0.4, alfa2 = alfa3, c3 = kc2, v2= kv1 e v4 = kv3
 % risolvendo il sistema troviamo alfa 2 e C2
 
-x0     = [C_1, pi/3];
-x_sol  = fsolve(sist, x0); % troviamo rispettivamente C_2 e alfa2
+x0     = [600, pi/4];
+x_sol  = fsolve(@sist, x0); % troviamo rispettivamente C_2 e alfa2
 C_2    = x_sol(1);
 alfa_2 = x_sol(2);
 
 %% first rotor exit - stator  inlet
 
-U_vec       = U*[1, 0];
-T_e_rot1_id = T_e_nzz - dh_rot1/c_p;
+
+T_e_rot1_id = T_e_nzz_re - dh_rot1/c_p;
 q_rot1_re   = (1 - k_blade^2)*(V_1^2 / 2) + (1 - k_n^2)*dh_rot1;
-T_e_rot1_re = T_e_rot1_id + q_nr/c_p;
+T_e_rot1_re = T_e_rot1_id + q_rot1_re/c_p;
 C_2_vec     = [C_2*cos(pi + alfa_2), C_2*sin(pi + alfa_2)];
 V_2_vec     = C_2_vec - U_vec;
 p_e_rot1    = p_e_nzz*(1 - dh_nz/(c_p*T_e_nzz_re))^(g/(g-1));
@@ -97,12 +102,12 @@ T_e_rot2_re = T_e_rot2_id + q_rot2_re/c_p;
 %% diagrammi
 
 figure;
-q1 = quiver(0,0,C_1*cos(alfa_1), C_1*sin(-alfa_1),1,'r');
-q1.MaxHeadSize = 0.05;
+q1 = quiver(0,0,C_1_vec(1), C_1_vec(2),1,'r');
+q1.MaxHeadSize = 0.09;
 hold on;
-q2 = quiver(0,0, V_1*cos(-beta_1), V_1*sin(-beta_1),1,'r');
+q2 = quiver(0,0, V_1_vec(1), V_1_vec(2),1,'r');
 q2.MaxHeadSize = 0.09;
-q3 = quiver(V_1*cos(-beta_1), V_1*sin(-beta_1), 1e-3*U, 0, 1, 'b');
+q3 = quiver(V_1_vec(1), V_1_vec(2), U, 0, 1, 'b');
 q2.MaxHeadSize = 0.08;
 grid on
 grid minor
@@ -113,21 +118,21 @@ q4.MaxHeadSize = 0.09;
 q5 = quiver(V_1_vec(1), V_1_vec(2), V_2_vec(1), V_2_vec(2), 1, 'r');
 q5.MaxHeadSize = 0.09;
 q6 = quiver(V_1_vec(1) + V_2_vec(1), V_1_vec(2) + V_2_vec(2), U, 0, 1, 'b');
-q6.MaxHeadSize = 0.08;
+q6.MaxHeadSize = 0.09;
 
 q7 = quiver(V_1_vec(1) + C_2_vec(1), V_1_vec(2) + C_2_vec(2), C_3_vec(1), C_3_vec(2), 1, 'r');
 q7.MaxHeadSize = 0.09;
 q8 = quiver(V_1_vec(1) + C_2_vec(1), V_1_vec(2) + C_2_vec(2), V_3_vec(1), V_3_vec(2), 1, 'r');
 q8.MaxHeadSize = 0.09;
 q9 = quiver(V_1_vec(1) + C_2_vec(1) + V_3_vec(1), V_1_vec(2) + C_2_vec(2) + V_3_vec(2), U, 0, 1, 'b');
-q9.MaxHeadSize = 0.08;
+q9.MaxHeadSize = 0.09;
 
 q10 = quiver(V_1_vec(1) + C_2_vec(1) + V_3_vec(1), V_1_vec(2) + C_2_vec(2) + V_3_vec(2), C_4_vec(1), C_4_vec(2), 1, 'r');
 q10.MaxHeadSize = 0.09;
 q11 =quiver(V_1_vec(1) + C_2_vec(1) + V_3_vec(1), V_1_vec(2) + C_2_vec(2) + V_3_vec(2), V_4_vec(1), V_4_vec(2), 1, 'r');
 q11.MaxHeadSize = 0.09;
 q12 = quiver(V_1_vec(1) + C_2_vec(1) + V_3_vec(1) + V_4_vec(1), V_1_vec(2) + C_2_vec(2) + V_3_vec(2) + V_4_vec(2), U, 0, 1, 'b');
-q12.MaxHeadSize = 0.08;
+q12.MaxHeadSize = 0.09;
 
 plot(linspace(-0.5, 1.5, 100),(C_1_vec(2) + C_2_vec(2))*ones(100,1), ':r', 'LineWidth',1);
 plot(linspace(-0.5, 1.5, 100), (C_1_vec(2) + C_2_vec(2) + C_3_vec(2))*ones(100,1), ':r', 'LineWidth',1);
