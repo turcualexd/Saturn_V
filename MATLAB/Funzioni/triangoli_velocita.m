f2a985a7185d9d11a6a23bccbfc6ac2d94fd57b8
turcualexd <turcualex01@gmail.com>
2023-05-10 12:59:54 +0200

    Aggiunto file di dati per triangoli velocità

--- MATLAB/Funzioni/triangoli_velocita.m
+++ MATLAB/Funzioni/triangoli_velocita.m
@@ -1,22 +1,15 @@
 clear, clc, close all;
 
-c1 = 1.1721e3;                      % m/s
-U = 244.1844;                       % m/s
-a1 = acos(4*U / c1);                % rad
-kn = 0.89;
-gamma = 1.128179;
-R = 294.4417;                       % J/kg*K
-T = 888.38;                         % K
+load("dati_triangoli_velocita.mat")
 
-c4 = 0.65 * sqrt(gamma * R * T);     % m/s
-a4 = pi/2;
-
-x0 = [600, pi/6];
-
-x = fsolve(@sist, x0);
+x0 = [600, pi/4];
+options = optimoptions('fsolve', 'MaxFunctionEvaluations', 100000, 'MaxIterations', 100000);
+x = fsolve(@sist, x0, options);
 
 c2 = x(1);      % m/s
 a2 = x(2);      % rad
 
 c3 = kn * c2;   % m/s
 a3 = a2;        % rad
+
+rad2deg(a2)
