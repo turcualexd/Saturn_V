c644fc7a63c44180f1a0d31f29531ee8b998921f
reshalwarna <reshalfernando19@gmail.com>
2023-07-20 12:04:19 +0200

    Create parametri_lanciatore.m

---
+++ MATLAB/Funzioni/parametri_lanciatore.m
@@ -0,0 +1,45 @@
+clear
+close all
+clc
+
+% Dati:
+% M_0: massa iniziale del sistema lanciatore             [kg]
+% M_u: massa carico utile del lanciatore                 [kg]
+%      coincide con la massa degli stadi superiori escluso il primo S-IC
+% M_s: massa della struttura del lanciatore              [kg]
+% M_p: massa del propellente per il primo stadio S-IC    [kg]
+% M_m: massa motore                                      [kg]
+% M_tank: massa tank                                     [kg]
+
+% M_0 = M_p + M_s + M_u
+
+M_0 = 0;
+M_u = 0;
+M_s = 0;
+M_p = 0;
+M_m = 0;
+M_tank = 0;
+
+% eps_u: indice di massa del carico utile
+eps_u = M_u / (M_0 - M_u);
+
+% csi_u: rapporto di massa del carico utile
+csi_u = M_u / M_0;
+
+% eps_s: indice strutturale delle masse inerti
+eps_s = M_s / (M_0 - M_u);
+
+% MR: mass ratio
+MR = eps_s * (1 - csi_u) + csi_u;
+
+% csi_p: frazione di propellente
+csi_p = 1 - MR;
+
+csi_p_controllo = M_p / M_0;
+
+% csi_m: rapporto di massa
+csi_m = M_m / M_0;
+
+% rapporto M_tank / M_p
+rapporto = M_tank / M_p;
+
