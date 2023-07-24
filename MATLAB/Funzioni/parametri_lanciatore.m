2cb0686e64b1a33ff3e86004b0bc172d13df6dc8
reshalwarna <reshalfernando19@gmail.com>
2023-07-24 11:37:00 +0200

    Parametri lanciatore: MATLAB + LaTeX

--- MATLAB/Funzioni/parametri_lanciatore.m
+++ MATLAB/Funzioni/parametri_lanciatore.m
@@ -11,33 +11,31 @@ clc
 % M_m: massa motore                                      [kg]
 % M_tank: massa tank                                     [kg]
 
-% M_0 = M_p + M_s + M_u
-
-M_0 = 2898941;
-M_u = 659293;
-M_s = 824511;
-M_p = 2074430;
-M_m = 8391.4588;
+% Dati iniziali ricavati da simulazione lancio e manuale F1 Rocketdyne
+M_0 = 2898941;         
+M_u = 824511;
+M_s = 242186;
+M_p = 2074430;         % 2656755 - 582325 kg
+M_m = 8445.4363;       % 18619 lb - manuale F1 schema pagina 10-
 M_tank = 124799.5412;
 
 % eps_u: indice di massa del carico utile
 eps_u = M_u / (M_0 - M_u);
 
 % csi_u: rapporto di massa del carico utile
-csi_u = M_u / M_0;
+zeta_u = M_u / M_0;
 
 % eps_s: indice strutturale delle masse inerti
 eps_s = M_s / (M_0 - M_u);
 
 % MR: mass ratio
-MR = eps_s * (1 - csi_u) + csi_u;
+MR = (M_0 - M_p) / M_0 ;
 
 % csi_p: frazione di propellente
+zeta_p = M_p / M_0;
 
-csi_p = M_p / M_0;
-
-% csi_m: rapporto di massa
-csi_m = M_m / M_0;
+% csi_m: rapporto di massa motore
+zeta_motore = 5*M_m / M_0;
 
 % rapporto M_tank / M_p
-rapporto = M_tank / M_p;
+rapporto_tank = M_tank / M_p;
