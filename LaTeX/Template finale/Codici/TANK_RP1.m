da7c6f61a5a5af42fc05fbcc7095f38e8c1ce64e
turcualexd <turcualex01@gmail.com>
2023-07-30 18:03:35 +0200

    Merge finale

--- LaTeX/Template finale/Codici/TANK_RP1.m
+++ LaTeX/Template finale/Codici/TANK_RP1.m
@@ -13,30 +13,30 @@ NPSH = 660; %inch
 
 %% volumi
 
-Ve = (2*pi*a^2*b)/3; 
-Vc = pi*a^2*lc;
-l_t = lc + 2*b;
-d_t = 17;
-V_t = 5*pi*(d_t/2)^2*l_t; %volume approssimato occupato dai tubi di lox
+V_e = (2*pi*a^2*b)/3;  %inch^3
+V_c = pi*a^2*lc;  %inch^3
+l_tubi = lc + 2*b;  %inch
+d_tubi = 17;  %inch
+V_tubi = 5*pi*(d_tubi/2)^2*l_tubi; %volume approssimato occupato dai tubi di lox %inch^3
 
-Vtot = 2*Ve + Vc - V_t;
+V_tot = 2*V_e + V_c - V_tubi;  %inch^3
 
-Vu = Vtot - Vt_p;
-Vu_1 = Ve - Vu; %prova che l'ullage è minore del volume dell'ellissoide, quindi bu meno di b
+V_u = V_tot - Vt_p;  %inch^3
+Vu_1 = V_e - V_u; %prova che l'ullage è minore del volume dell'ellissoide, quindi bu meno di b
 syms b_2
-bu = solve( Vu == (2*pi*a^2*b_2)/3, b_2); % altezza ullage inch  90.9363
+H_u = solve( V_u == (2*pi*a^2*b_2)/3, b_2); % altezza ullage inch  90.9363
 
 %% pressioni
-H_in = lc + 2*b - bu; %inch
-H = H_in/39.37;
+H_in = lc + 2*b - H_u; %inch
+H = H_in/39.37; %m
  
-pu = 25.2 ; %pressione ullage psia
+p_u = 25.2 ; %pressione ullage psia
 
 pi_n = rhoRP1*g*H;
 %p_i = pi_n /6895 % psia
 
 p_i = 13.38; %psia
-pt = p_i + pu; %psia
+p_t = p_i + p_u; %psia
 
 %pt = 40.13; %psi
 
@@ -59,52 +59,29 @@ k = a/b; %elipse ratio
 K = 0.8; % stress factor
 R = a*k;
 
-tk = (K*pt*a)/(S_y); %spessore nodo
-tcr = (pt*R)/(2*S_y); %spessore corona
+t_e = (p_t*a*(K+0.5*k))/(2*S_y); %spessore elissoide %inch
+t_c = (p_t*a)/(S_y); %spessore cilindro %inch
 
-te = (tk+tcr)/2; %spessore equivalente di un ellissoidale (pt*a*(K+0.5*k))/(2*S_y)
-tc = (pt*a)/(S_y); %spessore cilindro
+%%
+t_e_m = t_e*25.4; %mm 
+t_c_m = t_c*25.4; %mm
 
 %% aree
 
 e = (sqrt(a^2-b^2))/a; % eccentricità
 
-Ae = a^2 + (pi*b^2*log((1+e)/(1-e)))/(2*e); %area ellissoidale
+A_e = a^2 + (pi*b^2*log((1+e)/(1-e)))/(2*e); %area ellissoidale
 
-Ac = 2*pi*a*lc;
+A_c = 2*pi*a*lc;
 
-Atot = 2*Ae + Ac;
+A_tot = 2*A_e + A_c; %inch^2
 
 %% peso
 
 
 E1 = 2*k+(1/sqrt(k^2-1))*log((k+sqrt(k^2-1))/(k-sqrt(k^2-1))); %design factor
-We = (pi*a^2*te*E1*rho)/(2*k); 
+W_e = (pi*a^2*t_e*E1*rho)/(2*k); 
 
-Wc = 2*pi*a*lc*tc*rho; 
+W_c = 2*pi*a*lc*t_c*rho; 
 
-
-%% pressioni critiche dovute ai carichi esterni
-
-Cb = 0.10; %buckling coefficient da 0.05 a 0.10
-Pcre = (Cb*2*E*te^2)/(R^2); 
-
-if lc < 4.9*a*sqrt(a/tc)
-    Pcrc = 0.807*((E*tc^2)/(lc*a))*((1/(1-v^2))^3*(tc^2/a^2))^(1/4);
-else
-    Pcrc = (E*tc^3)/(4*(1-v^2)*a^3);
-end
-
-
-%%
-
-
-tk_m = tk/39.37
-tcr_m = tcr/39.37
-te_m = te/39.37 
-tc_m = tc/39.37
-
-
-
-
-
+PesoRP1 = 2*W_e + W_c; %lb
